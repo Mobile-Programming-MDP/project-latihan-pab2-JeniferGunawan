7000cacc2c04a65dd8704484b4e24a53c0993cb5
Michael <jenifergunawan5@gmail.com>
2025-04-03 15:33:05 +0700

    ADD : home screen

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -16,11 +16,19 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot){
+          if(snapshot.hasData){
+            return const HomeScreen();
+          }else{
+            return const SignInScreen();
+          }
+      }),
     );
   }
 }
