8894fe494d0083ada785e2b41cc96a0dd13e1c74
Michael <jenifergunawan5@gmail.com>
2025-04-03 15:45:57 +0700

    ADD : Sign In Screen

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -1,28 +1,32 @@
 // main.dart
+import 'package:fasum/screens/home_screen.dart';
+import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Fasum',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot){
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
           if(snapshot.hasData){
             return const HomeScreen();
           }else{
