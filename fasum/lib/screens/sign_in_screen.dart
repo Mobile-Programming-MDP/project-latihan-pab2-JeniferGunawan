66d433dc01e465ebb18bab14888f1098d2f74dd9
Michael <jenifergunawan5@gmail.com>
2025-04-03 15:14:41 +0700

    ADD : Sign Up Screen

---
+++ fasum/lib/screens/sign_in_screen.dart
@@ -0,0 +1,17 @@
+// screens/sign_in_screen.dart
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen ({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
